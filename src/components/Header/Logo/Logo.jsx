d9d32c82041c86ba70219587f61930721bfc26f1
YuriySW <yuriy.s@my.com>
2026-01-21 19:17:41 +0500

    hw9.2v1.1

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,10 +1,17 @@
 import style from './Logo.module.css';
-import logo from './img/logo.svg';
+// import logo from './img/logo.svg';
 
+// export const Logo = () => {
+//   return (
+//     <a className={style.link} href="/">
+//       <img className={style.logo} src={logo} alt="Blogget Logo" />
+//     </a>
+//   );
+// };
 export const Logo = () => {
   return (
     <a className={style.link} href="/">
-      <img className={style.logo} src={logo} alt="Blogget Logo" />
+      <img className={style.logo} src="/img/logo.svg" alt="Blogget Logo" />
     </a>
   );
 };
