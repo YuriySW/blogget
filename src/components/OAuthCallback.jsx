2a83455af5662667b8fc96805c4d954bb1bed4ef
YuriySW <yuriy.s@my.com>
2026-02-27 22:24:06 +0500

    404fix2.1

--- src/components/OAuthCallback.jsx
+++ src/components/OAuthCallback.jsx
@@ -1,5 +1,20 @@
-import {Preloader} from './Ul/Preloader/Preloader';
+// import {Preloader} from './Ul/Preloader/Preloader';
 
-const OAuthCallback = () => <Preloader />;
+// const OAuthCallback = () => <Preloader />;
+
+// export default OAuthCallback;
+
+// components/OAuthCallback/OAuthCallback.jsx
+import Layout from '../Layout';
+import Tabs from '../Main/Tabs';
+import style from '../Main/Main.module.css';
+
+const OAuthCallback = () => (
+  <main className={style.main}>
+    <Layout>
+      <Tabs />
+    </Layout>
+  </main>
+);
 
 export default OAuthCallback;
