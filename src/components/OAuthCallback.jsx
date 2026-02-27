7ad34785d1c6dcde87c8957c00014b1e9f884951
YuriySW <yuriy.s@my.com>
2026-02-27 22:32:08 +0500

    404fix2.2

--- src/components/OAuthCallback.jsx
+++ src/components/OAuthCallback.jsx
@@ -5,9 +5,9 @@
 // export default OAuthCallback;
 
 // components/OAuthCallback/OAuthCallback.jsx
-import Layout from '../Layout';
-import Tabs from '../Main/Tabs';
-import style from '../Main/Main.module.css';
+import Layout from './Layout/Layout';
+import Tabs from './Main/Tabs/Tabs';
+import style from './Main/Main.module.css';
 
 const OAuthCallback = () => (
   <main className={style.main}>
